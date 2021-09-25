d7eb63a262f562b6cd5ed25e99c99f6c2f00cb3c
peacebefore <42794888+peacebefore@users.noreply.github.com>
2021-09-24 22:39:00 -0700

    update fonts

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './assets/fonts/Atkinson_Hyperlegible/AtkinsonHyperlegible-Regular.ttf';
 import './assets/fonts/Thasadith/Thasadith-Regular.ttf';
 import './assets/fonts/Urbanist/Urbanist-VariableFont_wght.ttf';
 
